f779e5ec8282ce32ea09c4e20b08ead62a2ff200
sauravasphen <sauravasphen@gmail.com>
2021-04-13 14:10:18 +0545

    all files

---
+++ JS/menu.js
@@ -0,0 +1,10 @@
+function menuPull(){
+  var x = document.getElementById("menu");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  }
+  else {
+        x.style.display = "none";
+
+  }
+}
